55c4bf07c4d92fdf8a0c6ab63339ace1a8849427
Howard-Zhou <howard12345678987654321@hotmail.com>
2016-11-01 11:45:42 -0700

    facebook login initialized

--- components/login.js
+++ components/login.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { PropTypes, Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
@@ -19,6 +19,7 @@ const {
 import { Router, Scene } from 'react-native-router-flux';
 import { Actions } from 'react-native-router-flux';
 import * as Animatable from 'react-native-animatable';
+var {FBLogin, FBLoginManager,FBLoginView} = require('react-native-facebook-login');
 var Dimensions = require('Dimensions');
 var {
   width,
@@ -32,33 +33,114 @@ export default class Login extends Component {
     super(props);
     this.state = {
       uri:'',
+      user:'',
     }
   }
   componentWillMount(){
-    this.setState({uri:this.props.uri})
+      var _this = this
+    FBLoginManager.getCredentials(function(error, data){
+      console.warn(JSON.stringify(data))
+      if (!error) {
+        _this.setState({ user : data})
+      }
+    })
   }
+
+  handleLogin(){
+    var _this = this
+    FBLoginManager.login(function(error, data){
+      if (!error) {
+        console.warn(JSON.stringify(data))
+        _this.setState({ user : data});
+        // this.props.onLogin && _this.props.onLogin();
+      } else {
+        console.warn('wtf')
+        console.warn(JSON.stringify(data))
+        console.warn(error, data);
+      }
+    });
+  }
+
+
+    handleLogout(){
+      var _this = this
+      FBLoginManager.logout(function(error, data){
+        if (!error) {
+          _this.setState({ user : null});
+          // this.props.onLogout && _this.props.onLogout();
+        } else {
+          console.log(error, data);
+        }
+      });
+    }
+/*
+the commented code could be used for alternaive for another login mechanism
+*/
+
+/*
+login button:
+<FBLogin style={{ marginBottom: 10, }}
+  ref={(fbLogin) => { this.fbLogin = fbLogin }}
+  permissions={["email","user_friends"]}
+  loginBehavior={FBLoginManager.LoginBehaviors.Native}
+  onLogin={(data)=>this.onLogin(data)}
+  onLogout={()=>this.onLogout()}
+  onLoginFound={(data)=>this.onLoginFound(data)}
+  onLoginNotFound={()=>this.onLoginNotFound()}
+  onError={(data)=>this.onError(data)}
+  onCancel={()=>this.onCancel()}
+  onPermissionsMissing={(data)=>this.onPermissionsMissing(data)}
+/>
+*/
+
+/*
+login methods
+onLogin(data){
+  console.warn("logged in!")
+  console.warn(JSON.stringify(data))
+  this.setState({user:data.credentials})
+}
+
+onLogout(){
+  console.warn("logged out")
+  this.setState({user:null})
+}
+
+onLoginFound(data){
+  console.warn('existing login found')
+  console.warn(JSON.stringify(data))
+  this.setState({user:data.credentials})
+}
+
+onLoginNotFound(){
+  console.warn("no user logged in ")
+  this.setState({user:null})
+}
+
+onError(data){
+  console.warn("error")
+  console.warn(JSON.stringify(data))
+}
+
+onCancel(){
+  console.warn("user cancelled")
+}
+
+onPermissionsMissing(data){
+  console.warn("check permissions")
+  console.warn(JSON.stringify(data))
+}
+*/
 
   render(){
     return(
       <View style={styles.container}>
-        <LoginButton
-          publishPermissions={["publish_actions"]}
-          onLoginFinished={
-            (error, result) => {
-              if (error) {
-                alert("login has error: " + result.error);
-              } else if (result.isCancelled) {
-                alert("login is cancelled.");
-              } else {
-                AccessToken.getCurrentAccessToken().then(
-                  (data) => {
-                    alert(data.accessToken.toString())
-                  }
-                )
-              }
-            }
-          }
-          onLogoutFinished={() => alert("logout.")}/>
+        <TouchableOpacity onPress={this.handleLogin.bind(this)}>
+          <View style={{backgroundColor:'red',width:10,height:10}}></View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={this.handleLogout.bind(this)}>
+          <View style={{backgroundColor:'blue',width:10,height:10}}></View>
+        </TouchableOpacity>
       </View>
     )
   }
