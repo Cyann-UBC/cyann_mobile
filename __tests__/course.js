dd484c1c6c13d8f9a8cdbef3fd6eac0d552b0929
Howard-Zhou <howard12345678987654321@hotmail.com>
2016-12-02 21:29:45 -0800

    updated screenshot

--- __tests__/course.js
+++ __tests__/course.js
@@ -1,12 +1,12 @@
 import 'react-native';
 import React from 'react';
-import courseList from '../components/course';
+import course from '../components/course';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
   const tree = renderer.create(
-    <Index />
+    <course />
   );
 });
